75f304d6d9e9473d671e1993ec0b40f1a75967d7
Anshuman <makemefriend.anshu@gmail.com>
2016-08-12 00:28:03 +0530

    Add files via upload

---
+++ TestLibrary/TestExecutable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//by default namespace takes the name of the application, untill and unless you change the namespace name
+using TestLibrary;
+
+namespace TestExecutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test testObjRef = new Test();
+            string output = testObjRef.SayHello("Joydip");
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ TestLibrary/TestLibrary/Test.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TestLibrary
+{
+    /// <summary>
+    /// Sample test class
+    /// </summary>
+    public class Test
+    {
+        /// <summary>
+        /// Sample method to greet 
+        /// </summary>
+        /// <param name="name">name of the person to be greeted</param>
+        /// <returns>returns greeting statement</returns>
+        public string SayHello(string name)
+        {
+            return "Welcome to CSharp " + name;
+        }
+    }
+}
